e1c4353471b94c760d946c6f056a788cfa6a7756
Rafi <89698848+salirafi@users.noreply.github.com>
2026-03-21 15:48:42 +0900

    Delete data/download_data.py

--- data/download_data.py
+++
@@ -1,4 +0,0 @@
-import kagglehub
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parents[1]
-kagglehub.dataset_download("irkaal/foodcom-recipes-and-reviews", output_dir=BASE_DIR / "data" / "raw")
